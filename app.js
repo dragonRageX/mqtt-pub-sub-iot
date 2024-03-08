b699909f3421f451e5213fc282672cbfd2c8e15b
param <param772003@gmail.com>
2024-03-08 20:47:54 +0530

    new changes

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let app = express();
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 let mqttHandler = require("./mqtt_handler");
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 let mqttClient = new mqttHandler();
 mqttClient.connect();
